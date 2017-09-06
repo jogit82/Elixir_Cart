84183d8ac869445f2a034fb86b8a45f56fc79475
jogit82 <jo.c.keller@gmail.com>
2017-09-06 14:22:08 -0700

    added tests

--- lib/cart.ex
+++ lib/cart.ex
@@ -1,4 +1,11 @@
 defmodule Cart do
+
+  def handle({:item_added, _}) do #only use this function if item_added is passed in as the first argument, can't do this in any other language
+  end
+
+  def handle({:removed, _}) do #only use this function if item_added is passed in as the first argument, can't do this in any other language
+  end
+
   @moduledoc """
   Documentation for Cart.
   """
@@ -12,7 +19,7 @@ defmodule Cart do
       :world
 
   """
-  def hello do
-    :world
-  end
+  # def hello do
+  #   :world
+  # end
 end
--- test/cart_test.exs
+++ test/cart_test.exs
@@ -1,8 +1,25 @@
 defmodule CartTest do
   use ExUnit.Case
-  doctest Cart
 
-  test "greets the world" do
-    assert Cart.hello() == :world
+  # Example cart:
+  # cart = %{contents: [:item, :item2]}
+  # cart.contents = [:item1, :item2]
+
+  test "on empty cart is empty" do
+    empty_cart = %{}
+    assert empty_cart == %{}
+  end 
+
+  test "adds an item to a cart" do
+    empty_cart = %{}
+    cart = Cart.handle({:item_added, :my_item})
+    assert cart.contents == [:my_item]
+    # TODO: make a function and pass in a tuple. 
   end
+
+
+  # test "greets the world" do
+  #   assert Cart.hello() == :world
+  # end
+  
 end
